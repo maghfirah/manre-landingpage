cd6f70b81614cc5ed8ff64fbd6ead529cf60441d
firah <maghfirahsuyuti@uii.ac.id>
2020-08-25 16:05:08 +0700

    fixing animate on scrolling

--- manre-script.js
+++ manre-script.js
@@ -34,6 +34,7 @@ $(window).on("scroll", function() {
         $('nav').addClass('black');
         $('nav').removeClass('hide');
         $('.header-overlay').addClass('hide');
+
     }
 
     else {
@@ -51,26 +52,6 @@ $(document).ready(function(){
 
     // Add smooth scrolling on all links inside the navbar
     $(".manre-navbar a").on('click', function(event) {
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
-
-            // Store hash
-            var hash = this.hash;
-
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function(){
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
-        }  // End if
-    });
-    $(".manre-navbar button").on('click', function(event) {
         // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
             // Prevent default anchor click behavior
